1ffbed621efbb222c5cfa9f5ae0479a6829f594c
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-03-01 11:44:12 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+    {
+  char operator;
+  double operand1, operand2;
+
+  printf("Enter operator (+, -, *, /, ): ");
+  scanf("%c", &operator);
+
+  printf("Enter operands (numbers): ");
+
+  return 0;
+
+    }

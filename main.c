d5aca929c9249d4c1927e78b79792d6e7da825fc
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-03-01 19:42:20 +0530

    first commit

--- main.c
+++ main.c
@@ -9,6 +9,43 @@ int main(void)
   scanf("%c", &operator);
 
   printf("Enter operands (numbers): ");
+    scanf("%lf %lf", &operand1, &operand2);
+
+  switch (operator) {
+    case '+':
+      printf("%f + %f = %f\n",
+        operand1,
+        operand2,
+        operand1 + operand2
+      );
+    break;
+
+    case '-':
+      printf("%f + %f = %f\n",
+        operand1,
+        operand2,
+        operand1 + operand2);
+    break;
+
+    case '*':
+      printf("%f * %f = %f\n",
+        operand1,
+        operand2,
+        operand1 * operand2);
+    break;
+
+    case '/':
+      printf("%f / %f = %f\n",
+        operand1,
+        operand2,
+        operand1 / operand2);
+    break;
+
+    default:
+      printf("Invalid operator entered.\n");
+
+
+  }
 
   return 0;
 
